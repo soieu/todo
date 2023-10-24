6e6f0d41178cf5c83145b588ee960eb4cfff80e7
soieu <soieu9898@gmail.com>
2023-10-24 15:38:04 +0900

    feat: update Todo

--- src/main/java/com/plan/todo/service/TodoService.java
+++ src/main/java/com/plan/todo/service/TodoService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Slf4j
 @Service
@@ -21,19 +22,50 @@ public class TodoService {
         return savedEntity.getTitle();
     }
 
+    /**
+     *
+     * @param entity
+     * @return
+     * 새 Todo 생성
+     */
     public List<TodoEntity> create(final TodoEntity entity) {
         validate(entity);
+
         repository.save(entity);
         log.info("Entity Id: {} is saved.", entity.getId());
 
-        return repository.findByUserId(entity.getUserId());
+        return retrieve(entity.getUserId());
 
     }
+
+    /**
+     *
+     * @param userId
+     * @return
+     * userId가 동일한 Todo 리스트 검색
+     */
     public List<TodoEntity> retrieve(final String userId) {
         return repository.findByUserId(userId);
     }
 
+    /**
+     *
+     * @param entity
+     * @return
+     * Todo 내용 변경
+     */
+    public List<TodoEntity> update(final TodoEntity entity) {
+        validate(entity);
 
+        final Optional<TodoEntity> original = repository.findById(entity.getId());
+        original.ifPresent(todo -> {
+            todo.setTitle(entity.getTitle());
+            todo.setDone(entity.isDone());
+
+            repository.save(todo);
+        });
+        return retrieve(entity.getUserId());
+    }
 
     private void validate(final TodoEntity entity) {
         if (entity == null) {
