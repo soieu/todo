162f0c8da70e7256a5dd9025451756ddded41ea9
soieu <soieu9898@gmail.com>
2023-10-24 15:09:42 +0900

    feat: create todo

--- src/main/java/com/plan/todo/controller/TodoController.java
+++ src/main/java/com/plan/todo/controller/TodoController.java
@@ -1,15 +1,16 @@
 package com.plan.todo.controller;
 
 import com.plan.todo.dto.ResponseDTO;
+import com.plan.todo.dto.TodoDTO;
+import com.plan.todo.model.TodoEntity;
 import com.plan.todo.service.TodoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("todo")
@@ -24,6 +25,29 @@ public class TodoController {
         list.add(str);
         ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
         return ResponseEntity.ok().body(response);
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO dto) {
+        try {
+            String temporaryUserId = "temporary-user";
+
+            TodoEntity entity = TodoDTO.toEntity(dto);
+            entity.setId(null);
+            entity.setUserId(temporaryUserId);
+
+            List<TodoEntity> entities = service.create(entity);
+
+            List<TodoDTO> dtos = entities.stream().map(TodoDTO::new).collect(Collectors.toList());
+
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().data(dtos).build();
+
+            return ResponseEntity.ok().body(response);
+        }catch (Exception e) {
+            String error = e.getMessage();
+            ResponseDTO<TodoDTO> response = ResponseDTO.<TodoDTO>builder().error(error).build();
+            return ResponseEntity.badRequest().body(response);
+        }
 
     }
 }
--- src/main/java/com/plan/todo/service/TodoService.java
+++ src/main/java/com/plan/todo/service/TodoService.java
@@ -2,18 +2,42 @@ package com.plan.todo.service;
 
 import com.plan.todo.model.TodoEntity;
 import com.plan.todo.persistence.TodoRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Slf4j
 @Service
 public class TodoService {
-   @Autowired
+    @Autowired
     private TodoRepository repository;
 
-   public String testService() {
-       TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
-       repository.save(entity);
-       TodoEntity savedEntity  = repository.findById(entity.getId()).get();
-       return savedEntity.getTitle();
-   }
+    public String testService() {
+        TodoEntity entity = TodoEntity.builder().title("My first todo item").build();
+        repository.save(entity);
+        TodoEntity savedEntity = repository.findById(entity.getId()).get();
+        return savedEntity.getTitle();
+    }
+
+    public List<TodoEntity> create(final TodoEntity entity) {
+        validate(entity);
+        repository.save(entity);
+        log.info("Entity Id: {} is saved.", entity.getId());
+
+        return repository.findByUserId(entity.getUserId());
+
+    }
+
+    private void validate(final TodoEntity entity) {
+        if (entity == null) {
+            log.warn("Entity cannot be null.");
+            throw new RuntimeException("Entity cannot be null.");
+        }
+        if (entity.getUserId() == null) {
+            log.warn("Unknown User.");
+            throw new RuntimeException("Unknown User.");
+        }
+    }
 }
