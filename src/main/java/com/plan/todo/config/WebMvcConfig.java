18691d268bc243fe7f4f4ba57c6b87cabbd7a496
soieu <soieu9898@gmail.com>
2023-10-28 20:36:58 +0900

    feat: application config & aws check & cors fix

--- src/main/java/com/plan/todo/config/WebMvcConfig.java
+++ src/main/java/com/plan/todo/config/WebMvcConfig.java
@@ -11,7 +11,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000","http://todo-ui-soieu.ap-northeast-2.elasticbeanstalk.com/", "http://soieu.net")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true)
